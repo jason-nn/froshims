7aa2a96520b34fd81dd4afaf6c107a62d9a26cbf
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-06 16:59:39 +0800

    Add registrants logic

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from email import message
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 SPORTS = ['basketball', 'soccer', 'frisbee']
+REGISTRANTS = {}
 
 
 @app.route('/')
@@ -15,7 +17,20 @@ def register():
     name = request.form.get('name')
     sport = request.form.get('sport')
 
-    if not name or sport not in SPORTS:
-        return render_template('failure.html')
-    else:
-        return render_template('success.html')
+    if not name:
+        return render_template('error.html', message='Missing name')
+
+    if not sport:
+        return render_template('error.html', message='Missing sport')
+
+    if sport not in SPORTS:
+        return render_template('error.html', message='Invalid sport')
+
+    REGISTRANTS[name] = sport
+
+    return redirect('/registrants')
+
+
+@app.route('/registrants')
+def registrants():
+    return render_template('registrants.html', registrants=REGISTRANTS)
