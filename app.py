295d064b8098b80588f3d85c39674741af1ce5bc
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-06 16:28:07 +0800

    Refactor

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = ['basketball', 'soccer', 'frisbee']
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', sports=SPORTS)
 
 
 @app.route('/register', methods=['POST'])
 def register():
-    if not request.form.get('name') or request.form.get('sport') not in ['basketball', 'soccer', 'frisbee']:
+    name = request.form.get('name')
+    sport = request.form.get('sport')
+
+    if not name or sport not in SPORTS:
         return render_template('failure.html')
     else:
         return render_template('success.html')
