aaba0f277a7b4440bf0bbe5e706667fb09f255ba
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-06 16:09:11 +0800

    Add validate logic for register

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not request.form.get('name') or request.form.get('sport') not in ['basketball', 'soccer', 'frisbee']:
+        return render_template('failure.html')
+    else:
+        return render_template('success.html')
